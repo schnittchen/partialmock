d4ca1b7c6d8dc00604b5758c03ce0529f6f90e0c
Schnittchen <schnittchen@das-labor.org>
2010-07-25 18:19:54 +0200

    filled in implementation; dropped restore from api

--- partialmock.rb
+++ partialmock.rb
@@ -1,8 +1,97 @@
 module PartialMock
 
+	#--
+	class Caretaker
+		attr_reader :object
+
+		@@ct_objects = {}
+		def self.by_object(obj)
+			@@ct_objects[obj.object_id]
+		end
+
+		#pattern could be e.g. "original <meth>": must contain "<meth>" and should contain a space
+		#create only one per obj
+		def initialize(obj, pattern)
+			@object = obj
+			@meta = class << obj; self; end
+			@originals = {}
+			@pattern = pattern
+			@@ct_objects[obj.object_id] = self
+		end
+
+		#block will be evaluated in instance
+		def hook(meth, &block)
+			save_original(meth)
+			@meta.instance_eval do
+				begin
+					remove_method meth
+				rescue NameError
+					#the method might be defined up in the inheritance chain
+				end
+				define_method(meth, &block)
+			end
+		end
+
+		def invoke_backup(meth, *args)
+			origmeth = @originals[meth]
+			#XXX do we test against the case in the following line?
+			raise "invoke_backup called on method that has not been hooked" if origmeth.nil?
+			@object.send(@originals[meth], *args)
+		end
+
+		def restore(meth)
+			origmeth = @originals[meth]
+			raise "restore called on method that has not been hooked" if origmeth.nil?
+			@originals.delete meth
+			@meta.instance_eval do
+				begin
+					remove_method meth
+				rescue NameError
+					#the method might be defined up in the inheritance chain
+				end
+				alias_method meth, origmeth
+				remove_method origmeth
+			end
+		end
+
+		#ct object must not be used after that
+		def restore_all
+			@originals.keys.each { |meth| restore(meth) }
+			@@ct_objects.delete @object.object_id
+		end
+
+		private
+		def save_original(meth)
+			return if @originals.has_key?(meth)
+			backup_name = @pattern.gsub("<meth>", meth.to_s)
+			@meta.instance_eval do
+				begin
+					alias_method backup_name, meth
+				rescue NameError => e
+					raise "unknown method #{meth} (original message: #{e.message})"
+				end
+			end
+			@originals[meth] = backup_name
+		end
+	end
+	#++
+	
 	#Register the current TestCase object. Call this from inside tc's setup method.
 	#This will modify tc's teardown method to call wipe.
 	def self.setup_for(tc)
+		raise "already setup for a TestCase object" unless @tcct.nil?
+		@mocks = {}
+		@caretakers = {}
+
+		@tcct = Caretaker.new(tc, "original <meth>")
+		@tcct.hook(:teardown) do
+			#in the instance here
+			tcct = PartialMock.instance_eval { @tcct }
+			result = tcct.invoke_backup(:teardown)
+			tcct.restore_all #explicitly restore TestCase method
+			PartialMock.wipe
+			result
+		end
 	end
 
 	#Define the given block as a mock method.
@@ -10,44 +99,92 @@ module PartialMock
 	#If in_instance is true, block will be evaluated in the instance the mock method has
 	#been hooked onto.
 	def self.define_mockmeth(slot, in_instance = false, &block)
+		assert_registered
+		raise "already have a mock method in slot #{slot}" if @mocks.has_key?(slot)
+		@mocks[slot] = [block, in_instance]
 	end
 
 	#The object for which the current mockmeth is called.
 	#Undefined if not inside a mock method with in_instance == false
 	def self.current_object
+		assert_registered
+		@current_object
 	end
 
 	#The method the current mockmeth is replacing.
 	#Undefined if not inside a mock method with in_instance == false
 	def self.current_method
+		assert_registered
+		@current_method
 	end
 
 	#Hook mock method denoted by slot onto obj as method meth.
 	#Saves the original method internally the first time called with the combination [obj, meth].
-	#If restore is true, the original method will be restored immediately prior to
-	#executing the mock method.
-	def self.hook(slot, obj, meth, restore = false)
+	def self.hook(slot, obj, meth)
+		raise "unknown slot #{slot}" unless @mocks.has_key?(slot)
+		ct = @caretakers[obj.object_id]
+		ct = @caretakers[obj.object_id] = Caretaker.new(obj, "saved method <meth>") if ct.nil?
+
+		block, in_instance = @mocks[slot]
+		if in_instance then
+			ct.hook(meth, &block)
+		else
+			obfusciated_block_variable = block #there will be no instance method with that name
+			ct.hook(meth) do |*args|
+				#we are in the instance obj now!
+				sulf = self
+				PartialMock.instance_eval do
+					@current_object = sulf
+					@current_method = __method__
+				end
+
+				begin
+					obfusciated_block_variable.call(*args)
+				ensure
+					PartialMock.instance_eval do
+						@current_object = nil
+						@current_method = nil
+					end
+				end
+			end
+		end
 	end
 
 	#Invoke obj's original meth method. Inside that method, __method__ will not equal meth!
 	#Only possible if obj's method meth is hooked.
 	def self.invoke_backup(obj, meth, *args)
+		ct = @caretakers[obj.object_id]
+		raise "invoke_backup called on method that has not been hooked" if ct.nil?
+		ct.invoke_backup(meth, *args)
 	end
 
 	#Restore the original method.
 	def self.restore(obj, meth)
+		assert_registered
+		ct = @caretakers[obj.object_id]
+		raise "restore called on method that has not been hooked" if ct.nil?
+		ct.restore(meth)
 	end
 
 	#Restore all original methods.
 	def self.restore_all(obj)
+		assert_registered
+		ct = @caretakers[obj.object_id]
+		@caretakers.delete obj.object_id
+		raise "restore_all called for object without hooked methods" if ct.nil?
+		ct.restore_all
 	end
 
 	#Obtain value from module hash.
 	def self.[](ky)
+		assert_registered
+		@hash[ky]
 	end
 
 	#Store value into module hash.
 	def self.[]=(ky, val)
+		assert_registered
+		@hash[ky] = val
 	end
 
 	#Reset module for next use:
@@ -56,5 +193,31 @@ module PartialMock
 	#* clear mock method slots
 	#* clear module hash
 	def self.wipe
+		assert_registered
+		@caretakers.each_value { |ct| ct.restore_all }
+		
+		clean_state
 	end
+
+	#--
+	#Setup the wiped/unregistered state internally
+	def self.clean_state
+		@hash = {}
+		@tcct = nil
+		@mocks = nil
+		@caretakers = nil
+		@current_object = nil
+		@current_method = nil
+	end
+
+	def self.assert_registered
+		raise "not set up for a TestCase object" if @tcct.nil?
+	end
+
+	class << self
+		private :clean_state
+	end
+
+	clean_state
+	#++
 end #module PartialMock
